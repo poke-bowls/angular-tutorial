b9372c6ea066e745dd466e88e157ac62fb62289f
Micah Koki <micah.koki@gmail.com>
2016-02-06 10:46:57 -1000

    add angular to html 5 page

--- server.js
+++ server.js
@@ -2,14 +2,9 @@
 
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8888;
 
 app.use(express.static('./public'));
-
-
-
-
-
 
 
 
