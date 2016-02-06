fa92a17ca4ec1018e09d9e408a208e84b269a186
Micah Koki <micah.koki@gmail.com>
2016-02-06 10:38:09 -1000

    add express static server

---
+++ server.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./public'));
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function(){
+  process.stdout.write(`server listening on port ${PORT}`);
+});
