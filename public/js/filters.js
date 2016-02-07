8623601b07b1d6c9d97bdd06e6e7d531ddc8c11a
Micah Koki <micah.koki@gmail.com>
2016-02-06 15:45:41 -1000

    add custom beforeYearFilter

---
+++ public/js/filters.js
@@ -0,0 +1,11 @@
+
+"use strict";
+
+angular.module('myApp')
+.filter('beforeYearFilter', function(){
+  return function(collection, year) {
+    return collection.filter(function(item){
+      return item.year < year;
+    });
+  };
+});
